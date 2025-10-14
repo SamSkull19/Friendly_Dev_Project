336d37286bb4e26f86197ac3074a6fb9229b2f2a
sifat-samin <sifatsamin19@gmail.com>
2025-10-14 22:10:30 +0600

    Project initialized

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Friendly DEV" },
+    { name: "description", content: "Custom Website Development" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>My APP</>;
 }
