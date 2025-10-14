19445ea855194b541a30dd184d6e071e1cdd7c7d
sifat-samin <sifatsamin19@gmail.com>
2025-10-14 22:33:11 +0600

    Routes Created

---
+++ app/routes/about/index.tsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-white mb-2">
+                He, I'm Samin
+            </h1>
+        </>
+    );
+};
+
+export default AboutPage;
---
+++ app/routes/home/index.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/index";
+
+export function meta({ }: Route.MetaArgs) {
+    return [
+        { title: "Friendly DEV | Home" },
+        { name: "description", content: "Custom Website Development" },
+    ];
+}
+
+export default function Home() {
+    return <>My APP</>;
+}
