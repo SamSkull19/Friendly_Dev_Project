9ae9f67fe873b1cde2dd0216c5a96e9342c3645a
sifat-samin <sifatsamin19@gmail.com>
2025-10-15 01:42:45 +0600

    Hero Section Added

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,3 +1,4 @@
+import Hero from "~/components/Hero";
 import type { Route } from "./+types/index";
 
 export function meta({ }: Route.MetaArgs) {
@@ -8,5 +9,5 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
-    return <>My APP</>;
+    return <Hero name="Samin"/>;
 }
