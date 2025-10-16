a8a6f95f1023165787b4ef891a501e2ae9a7698e
sifat-samin <sifatsamin19@gmail.com>
2025-10-17 02:28:03 +0600

    Form Modified

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,42 +1,42 @@
 import type { Route } from './+types/index';
-import { Form } from 'react-router';
+// import { Form } from 'react-router';
 
-export async function action({ request }: Route.ActionArgs) {
-    const formData = await request.formData();
+// export async function action({ request }: Route.ActionArgs) {
+//     const formData = await request.formData();
 
-    const name = formData.get('name') as string;
-    const email = formData.get('email') as string;
-    const subject = formData.get('subject') as string;
-    const message = formData.get('message') as string;
+//     const name = formData.get('name') as string;
+//     const email = formData.get('email') as string;
+//     const subject = formData.get('subject') as string;
+//     const message = formData.get('message') as string;
 
-    const errors: Record<string, string> = {}
+//     const errors: Record<string, string> = {}
 
-    // Error Set
-    if (!name) errors.name = 'Name is Required.';
+//     // Error Set
+//     if (!name) errors.name = 'Name is Required.';
 
-    if (!email) {
-        errors.email = 'Email is Required.';
-    }
-    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        errors.email = 'Invalid Email format.';
-    }
+//     if (!email) {
+//         errors.email = 'Email is Required.';
+//     }
+//     else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+//         errors.email = 'Invalid Email format.';
+//     }
 
-    if (!subject) errors.subject = 'Subject is Required.';
-    if (!message) errors.message = 'Message is Required.';
+//     if (!subject) errors.subject = 'Subject is Required.';
+//     if (!message) errors.message = 'Message is Required.';
 
-    if (Object.keys(errors).length > 0) {
-        return { errors };
-    }
+//     if (Object.keys(errors).length > 0) {
+//         return { errors };
+//     }
 
-    const data = {
-        name, email, subject, message,
-    };
+//     const data = {
+//         name, email, subject, message,
+//     };
 
-    return { message: 'Form submitted successfully!', data };
-}
+//     return { message: 'Form submitted successfully!', data };
+// }
 
 const ContactPage = ({ actionData }: Route.ComponentProps) => {
-    const errors = actionData?.errors || {};
+    // const errors = actionData?.errors || {};
 
     return (
         <section className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900'>
@@ -44,16 +44,11 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                 📬 Contact Me
             </h2>
 
-            {
-                actionData?.message ? (
-                    <p className='mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-sm'>
-                        {actionData.message}
-                    </p>
-                ) : null
-            }
-
-            <Form method='post' className='space-y-6'>
-                {/* Full Name */}
+            <form
+                method='POST'
+                action='https://formspree.io/f/movkrjoa'
+                className='space-y-6'
+            >
                 <div>
                     <label
                         htmlFor='name'
@@ -61,7 +56,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                     >
                         Full Name
                     </label>
-
                     <input
                         type='text'
                         id='name'
@@ -69,11 +63,7 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                         className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
                     />
                 </div>
-                {
-                    errors.name && (<p className='text-red-400 text-sm mt-1'>{errors.name}</p>)
-                }
 
-                {/* Email */}
                 <div>
                     <label
                         htmlFor='email'
@@ -81,7 +71,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                     >
                         Email Address
                     </label>
-
                     <input
                         type='email'
                         id='email'
@@ -89,11 +78,7 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                         className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
                     />
                 </div>
-                {
-                    errors.email && (<p className='text-red-400 text-sm mt-1'>{errors.email}</p>)
-                }
 
-                {/* Subject */}
                 <div>
                     <label
                         htmlFor='subject'
@@ -101,7 +86,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                     >
                         Subject
                     </label>
-
                     <input
                         type='text'
                         id='subject'
@@ -109,11 +93,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                         className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
                     />
                 </div>
-                {
-                    errors.subject && (<p className='text-red-400 text-sm mt-1'>{errors.subject}</p>)
-                }
-
-                {/* Message */}
                 <div>
                     <label
                         htmlFor='message'
@@ -121,7 +100,6 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                     >
                         Message
                     </label>
-
                     <textarea
                         id='message'
                         name='message'
@@ -129,18 +107,13 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
                         className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
                     />
                 </div>
-                {
-                    errors.message && (<p className='text-red-400 text-sm mt-1'>{errors.message}</p>)
-                }
-
-                {/* Submit Button */}
                 <button
                     type='submit'
                     className='w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition'
                 >
                     Send Message
                 </button>
-            </Form>
+            </form>
         </section>
     );
 };
