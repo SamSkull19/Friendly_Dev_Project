f5e101c9a63b36aeb3e5326c43140ad7eb68670f
sifat-samin <sifatsamin19@gmail.com>
2025-10-16 20:26:41 +0600

    Home Page Post API added

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/index";
-import type { PostData, Project, StrapiResponse, StrapiProject } from '~/types';
+import type { PostData, Project, StrapiResponse, StrapiProject, StrapiPost } from '~/types';
 import FeaturedProjects from '~/components/FeaturedProjects';
 import AboutPreview from '~/components/AboutPreview';
 import LatestPosts from '~/components/LatestPosts';
@@ -17,8 +17,13 @@ export async function loader({
     const url = new URL(request.url);
 
     const [projectRes, postRes] = await Promise.all([
-        fetch(`${import.meta.env.VITE_API_URL}/projects?filters[featured][$eq]=true&populate=*`),
-        fetch(new URL('/posts-meta.json', url)),
+        fetch(
+            `${import.meta.env.VITE_API_URL}/projects?filters[featured][$eq]=true&populate=*`
+        ),
+
+        fetch(
+            `${import.meta.env.VITE_API_URL}/posts?sort[0]=date:desc&pagination[limit]=3&populate=image`
+        ),
     ]);
 
     if (!projectRes.ok || !postRes.ok) {
@@ -26,7 +31,7 @@ export async function loader({
     }
 
     const projectJson: StrapiResponse<StrapiProject> = await projectRes.json();
-    const postsJson = await postRes.json();
+    const postsJson: StrapiResponse<StrapiPost> = await postRes.json();
 
     const projects = projectJson.data.map((item: any) => ({
         id: item.id,
@@ -42,7 +47,19 @@ export async function loader({
         featured: item.featured,
     }))
 
-    return { projects, posts: postsJson };
+    const posts = postsJson.data.map((item) => ({
+        id: item.id,
+        slug: item.slug,
+        title: item.title,
+        body: item.body,
+        excerpt: item.excerpt,
+        date: item.date,
+        image: item.image?.url
+            ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+            : '/images/no-image.png',
+    }));
+
+    return { projects, posts };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
