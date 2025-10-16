2debc6e6174979fdf09f61e3f9977ad4b3cff2b4
sifat-samin <sifatsamin19@gmail.com>
2025-10-16 18:55:03 +0600

    Strapi API added

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -6,10 +6,29 @@ import Pagination from '~/components/Pagination';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects?populate=*`);
 
-  return { projects: data }
+  if (!res.ok) {
+    throw new Error('Failed to fetch projects');
+  }
+
+  const json = await res.json();
+
+  const projects = json.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url
+      ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+      : '/images/no-image.png',
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  }))
+
+  return { projects }
 }
 
 
